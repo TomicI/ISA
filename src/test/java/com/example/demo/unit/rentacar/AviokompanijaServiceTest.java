eac78178edf935d355118f4a0765ff29a7ee3888
TomicI <ivanatomic3@uns.ac.rs>
2019-09-12 00:35:36 +0200

    test-dodatni

--- src/test/java/com/example/demo/unit/rentacar/AviokompanijaServiceTest.java
+++ src/test/java/com/example/demo/unit/rentacar/AviokompanijaServiceTest.java
@@ -1,6 +1,8 @@
 package com.example.demo.unit.rentacar;
 
+import com.dto.aviokompanija.AviokompanijaDTO;
 import com.model.RentACar;
+import com.model.aviokompanija.Aerodrom;
 import com.model.aviokompanija.Aviokompanija;
 import com.repository.RentACarRepository;
 import com.repository.aviokompanija.AviokompanijaRepository;
@@ -16,7 +18,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -33,13 +37,21 @@ public class AviokompanijaServiceTest {
     @Test
     public void getBy(){
         aviokompanija = new Aviokompanija(1L,"Aeroporto De Lisboa","Aviokompanija");
-        Mockito.when(aviokompanijaRepository.findById(1L)).thenReturn(Optional.of(aviokompanija));
+        when(aviokompanijaRepository.findById(1L)).thenReturn(Optional.of(aviokompanija));
         Aviokompanija aviokompanija = aviokompanijaService.findOne(1L);
         assertEquals(aviokompanija.getNaziv(),"Aeroporto De Lisboa");
 
 
     }
 
+    @Test
+    public void when_save_aviokompanija_it_should_return_aviokompanija(){
+        aviokompanija = new Aviokompanija(1L,"Aeroporto De Lisboa","Aviokompanija");
+        when(aviokompanijaRepository.save(aviokompanija)).thenReturn(new Aviokompanija());
+        AviokompanijaDTO created=aviokompanijaService.create(new AviokompanijaDTO(aviokompanija));
+        assertEquals(created.getNaziv(),(aviokompanija.getNaziv()));
+
+    }
 
 
 }
